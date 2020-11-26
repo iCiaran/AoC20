318419520f06a263b795ee56572295babda48339
iCiaran <iCiaran@users.noreply.github.com>
2020-11-26 01:14:17 +0000

    Start setting up

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/iCiaran/AoC20
 
 go 1.14
+
+require github.com/stretchr/testify v1.6.1
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/iCiaran/AoC20/day_00"
+)
+
+type challenge interface {
+	PartA(input string) string
+	PartB(input string) string
+}
+
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	fmt.Printf("%s took %s", name, elapsed)
+}
+
+func inputPath(day int) string {
+	return fmt.Sprintf("day_%02d/inputs/", day)
+}
+
+func runSingle(challenges map[int]challenge, day int, a, b bool) {
+	fmt.Printf("Day %d\n", day)
+	inputPath := inputPath(day)
+	if c, ok := challenges[day]; ok {
+		if a {
+			start := time.Now()
+			result := c.PartA(inputPath + "real_a.txt")
+			elapsed := time.Since(start)
+			fmt.Printf("├ A - %v - %s\n", elapsed, result)
+		} else {
+			fmt.Println("├ A - Skipped")
+		}
+		if b {
+			start := time.Now()
+			result := c.PartB(inputPath + "real_b.txt")
+			elapsed := time.Since(start)
+			fmt.Printf("└ B - %v - %s\n", elapsed, result)
+		} else {
+			fmt.Println("└ B - Skipped")
+		}
+	} else {
+		fmt.Println("└ Skipped")
+	}
+}
+
+func main() {
+	challenges := make(map[int]challenge, 25)
+	challenges[0] = day_00.New()
+
+	runSingle(challenges, 0, true, true)
+	for i := 1; i < 26; i++ {
+		runSingle(challenges, i, true, true)
+	}
+}
