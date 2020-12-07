cada1d39ba4e1178bd90c786ab81c950b77cc3d6
Ciaran Moran <iCiaran@users.noreply.github.com>
2020-12-07 23:09:27 +0000

    Day 07 (#8)
    A + B

---
+++ day_07/cmd/run/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/iCiaran/AoC20/day_07"
+)
+
+func main() {
+	day := day_07.New()
+	fmt.Println(day.PartA("../../inputs/real_a.txt"))
+	fmt.Println(day.PartB("../../inputs/real_b.txt"))
+}
---
+++ day_07/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd cmd/run
+go build -o day_07.out .  && ./day_07.out
+cd - > /dev/null
