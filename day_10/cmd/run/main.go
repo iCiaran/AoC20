641d35e33b936993b31433af0cd966a36b97864d
Ciaran Moran <iCiaran@users.noreply.github.com>
2020-12-10 18:13:20 +0000

    Day 10 (#11)
    A + B

---
+++ day_10/cmd/run/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/iCiaran/AoC20/day_10"
+)
+
+func main() {
+	day := day_10.New()
+	fmt.Println(day.PartA("../../inputs/real_a.txt"))
+	fmt.Println(day.PartB("../../inputs/real_b.txt"))
+}
---
+++ day_10/day_10_test.go
@@ -0,0 +1,83 @@
+package day_10
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPartA(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{
+			input: "inputs/test_01.txt",
+			want:  "35",
+		},
+		{
+			input: "inputs/test_02.txt",
+			want:  "220",
+		},
+		{
+			input: "inputs/real_a.txt",
+			want:  "2100",
+		},
+	}
+
+	day := New()
+	for i, test := range tests {
+		t.Run(fmt.Sprint("test_", i), func(t *testing.T) {
+			got := day.PartA(test.input)
+
+			assert.Equal(test.want, got)
+		})
+	}
+}
+
+func TestPartB(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{
+			input: "inputs/test_01.txt",
+			want:  "8",
+		},
+		{
+			input: "inputs/test_02.txt",
+			want:  "19208",
+		},
+		{
+			input: "inputs/real_b.txt",
+			want:  "16198260678656",
+		},
+	}
+	day := New()
+	for i, test := range tests {
+		t.Run(fmt.Sprint("test_", i), func(t *testing.T) {
+			got := day.PartB(test.input)
+
+			assert.Equal(test.want, got)
+		})
+	}
+}
+
+func BenchmarkPartA(b *testing.B) {
+	day := New()
+	for n := 0; n < b.N; n++ {
+		day.PartA("inputs/real_a.txt")
+	}
+}
+
+func BenchmarkPartB(b *testing.B) {
+	day := New()
+	for n := 0; n < b.N; n++ {
+		day.PartB("inputs/real_b.txt")
+	}
+}
