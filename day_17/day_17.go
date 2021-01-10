0f627e17eb9c4596235727ad29cac4d046367342
iCiaran <iCiaran@users.noreply.github.com>
2021-01-10 19:33:24 +0000

    Day 17
    A + B

--- day_17/day_17.go
+++ day_17/day_17.go
@@ -4,7 +4,15 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strconv"
 )
+
+type coord struct {
+	x int
+	y int
+	z int
+	w int
+}
 
 type Day_17 struct{}
 
@@ -30,10 +38,101 @@ func getLines(filepath string) []string {
 	return lines
 }
 
+// func getNeighbours(c coord) []coord {
+// 	neighbours := make([]coord, 0, 26)
+
+// 	for x := -1; x < 2; x++ {
+// 		for y := -1; y < 2; y++ {
+// 			for z := -1; z < 2; z++ {
+// 				if !(x == 0 && y == 0 && z == 0) {
+// 					neighbours = append(neighbours, coord{c.x + x, c.y + y, c.z + z, 0})
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	return neighbours
+// }
+
+func getNeighbours(c coord, hyper bool) []coord {
+	neighbours := make([]coord, 0, 26)
+
+	for x := -1; x < 2; x++ {
+		for y := -1; y < 2; y++ {
+			for z := -1; z < 2; z++ {
+				if hyper {
+					for w := -1; w < 2; w++ {
+						if !(x == 0 && y == 0 && z == 0 && w == 0) {
+							neighbours = append(neighbours, coord{c.x + x, c.y + y, c.z + z, c.w + w})
+						}
+					}
+				} else {
+					if !(x == 0 && y == 0 && z == 0) {
+						neighbours = append(neighbours, coord{c.x + x, c.y + y, c.z + z, 0})
+					}
+				}
+			}
+		}
+	}
+
+	return neighbours
+}
+
+func countActive(coords []coord, active map[coord]bool) int {
+	count := 0
+
+	for _, c := range coords {
+		if active[c] {
+			count++
+		}
+	}
+
+	return count
+}
+
+func cycle(active map[coord]bool, hyper bool) map[coord]bool {
+	nextActive := make(map[coord]bool)
+
+	for current := range active {
+		neighbours := getNeighbours(current, hyper)
+
+		activeNeighbours := countActive(neighbours, active)
+		if activeNeighbours == 2 || activeNeighbours == 3 {
+			nextActive[current] = true
+		}
+
+		for _, neighbour := range neighbours {
+			if !active[neighbour] && countActive(getNeighbours(neighbour, hyper), active) == 3 {
+				nextActive[neighbour] = true
+			}
+		}
+	}
+
+	return nextActive
+}
+
+func runBootProcess(input string, hyper bool) map[coord]bool {
+	active := make(map[coord]bool)
+
+	for y, line := range getLines(input) {
+		for x, c := range line {
+			if c == '#' {
+				active[coord{x, y, 0, 0}] = true
+			}
+		}
+	}
+
+	for i := 0; i < 6; i++ {
+		active = cycle(active, hyper)
+	}
+
+	return active
+}
+
 func (d *Day_17) PartA(input string) string {
-    return "A"
+	return strconv.Itoa(len(runBootProcess(input, false)))
 }
 
 func (d *Day_17) PartB(input string) string {
-	return "B"
+	return strconv.Itoa(len(runBootProcess(input, true)))
 }
--- day_17/day_17_test.go
+++ day_17/day_17_test.go
@@ -2,8 +2,9 @@ package day_17
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPartA(t *testing.T) {
@@ -14,12 +15,16 @@ func TestPartA(t *testing.T) {
 		want  string
 	}{
 		{
+			input: "inputs/test_01.txt",
+			want:  "112",
+		},
+		{
 			input: "inputs/real_a.txt",
-			want:  "A",
+			want:  "333",
 		},
 	}
 
-    day := New()
+	day := New()
 	for i, test := range tests {
 		t.Run(fmt.Sprint("test_", i), func(t *testing.T) {
 			got := day.PartA(test.input)
@@ -37,11 +42,15 @@ func TestPartB(t *testing.T) {
 		want  string
 	}{
 		{
+			input: "inputs/test_01.txt",
+			want:  "848",
+		},
+		{
 			input: "inputs/real_b.txt",
-			want:  "B",
+			want:  "2676",
 		},
 	}
-    day := New()
+	day := New()
 	for i, test := range tests {
 		t.Run(fmt.Sprint("test_", i), func(t *testing.T) {
 			got := day.PartB(test.input)
@@ -52,14 +61,14 @@ func TestPartB(t *testing.T) {
 }
 
 func BenchmarkPartA(b *testing.B) {
-    day := New()
+	day := New()
 	for n := 0; n < b.N; n++ {
 		day.PartA("inputs/real_a.txt")
 	}
 }
 
 func BenchmarkPartB(b *testing.B) {
-    day := New()
+	day := New()
 	for n := 0; n < b.N; n++ {
 		day.PartB("inputs/real_b.txt")
 	}
